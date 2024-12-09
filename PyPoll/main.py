8bb8e700a7fae9d659ca5c16c19784ac35c42a9c
alexvallone24 <vallonecarly@gmail.com>
2024-12-08 22:36:21 -0600

    Added comments and ran and debugged code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,19 @@
+# Import and set file path
 import os
 import csv
 
 file_path = "C:/Users/vallo/Desktop/Homework/Python_Challenge/PyPoll/Resources/election_data.csv"
 
+# Read csv
 with open(file_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
 
+   # Set variables for counting votes
     candidate_votes = {}
     total_votes = 0
     
+   # Count votes for each row in csv
     for row in csvreader:
         candidate =  row[2]
         total_votes += 1
@@ -20,17 +24,19 @@ with open(file_path, newline='') as csvfile:
             candidate_votes[candidate] = 1
         
 
-
+# Create results list
 results = []
 results.append("Election Results")
 results.append("--------------")
 results.append(f"Total Votes: {total_votes}")
 results.append("---------------")
 
+# Set variables for winner
 winner = ""
 candidate_percentage = {}
 winning_count = 0
 
+# Calculate winner and each candidate's percentage
 for candidate, votes in candidate_votes.items():
     percentage = ((votes / total_votes) * 100)
     candidate_percentage[candidate] = percentage
@@ -39,10 +45,12 @@ for candidate, votes in candidate_votes.items():
         winning_count = votes
         winner = candidate
 
+# Add to results list
 results.append("-------------")
 results.append(f"Winner: {winner}")
 results.append("--------------")
 
+# Print and set a file location for results
 for line in results:
     print(line)
 
